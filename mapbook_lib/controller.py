24c7b4fa20990786bde083e83b05bdc9d5f6b11b
Klaudia0100 <klaudia.kurowska@student.wat.edu.pl>
2026-05-03 19:21:20 +0200

    feat: add location function

--- mapbook_lib/controller.py
+++ mapbook_lib/controller.py
@@ -1,20 +1,22 @@
+import requests
+from bs4 import BeautifulSoup
+import folium
+
 def read_data(users_data: list) -> None:
     for user in users_data:
-        print(
-            f'twój znajomy {user['username']} z miejscowości {user['location']} opubikował {user['posts']} wiadomości. Ostatnia wiadomość {user['usermassage'][-1]}')
+        print(f'twój znajomy {user['username']} z miejscowości {user['location']} opublikował {user['posts']} wiadomości. Ostatnia wiadomość {user['usermessage'][-1]}')
 
 
 def add_user(users_data: list) -> None:
-    name = input('Podaj imie: ')
+    name = input('Podaj imię: ')
     location = input('Podaj lokalizację: ')
     posts = int(input('Podaj liczbę postów: '))
-    usermassage = ['']
-    users_data.append({'username': name, 'location': location, 'posts': posts,
-                       'usermassage': usermassage})
+    usermessage = ['']
+    users_data.append({'username': name, 'location': location, 'posts': posts, 'usermessage': usermessage})
 
 
 def remove_user(users_data: list) -> None:
-    name = input('Podaj imie użytownika do usunięcia: ')
+    name = input('Podaj imię użytkownika do usunięcia: ')
 
     for user in users_data:
         if user['username'] == name:
@@ -22,10 +24,35 @@ def remove_user(users_data: list) -> None:
 
 
 def update_user(users_data: list) -> None:
-    name = input('Podaj imie użytkownika do zmiany: ')
+    name = input('Podaj imię użytkownika do zmiany: ')
 
     for user in users_data:
         if user['username'] == name:
-            user['username'] = input('Podaj nowe imie: ')
+            user['username'] = input('Podaj nowe imię: ')
             user['location'] = input('Podaj nową lokalizację: ')
             user['posts'] = int(input('Podaj liczbę postów: '))
+
+
+def get_coordinates(location: str) -> list:
+    url = f"https://pl.wikipedia.org/wiki/{location}"
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
+
+    response = requests.get(url, headers=headers)
+    response_html = BeautifulSoup(response.text, 'html.parser')
+    response_html_latitude = float(response_html.select('.latitude')[1].text.replace(',', '.'))
+    response_html_longitude = float(response_html.select('.longitude')[1].text.replace(',', "."))
+    return [response_html_latitude, response_html_longitude]
+
+
+def get_mapa(users_data: list) -> None:
+    m = folium.Map([52, 21], zoom_start=6)
+    for user in users_data:
+        folium.Marker(
+            location=get_coordinates(user['location']),
+            tooltip="Click me!",
+            popup=user['username'],
+            icon=folium.Icon(icon="cloud"),
+        ).add_to(m)
+
+    m.save('mapa.html')
